6a3239ef6f1b4c1b97aad4e488980dc014f7c951
Joanna Fengler <asiapobuta@wp.pl>
2018-03-13 05:41:15 +0100

    triangle field task js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Hello.
+//
+// This is a triangle field - first task.
+
+/*alert var name = prompt('Enter your name');
+('Hello, ' + name);
+console.log('Hello, ' + name);*/
+var a = 6;
+var h = 5;
+var triangleArea = (a *h /2);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
